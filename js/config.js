9f929cbcdc833946ca808ab1269f12406df92bdd
Chirpy1230 <erignacio1@up.edu.ph>
2026-09-16 23:06:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 export const CONFIG = {
   SUPABASE_URL: 'https://cwbrzxqmlzgedaisaour.supabase.co',
+  // Note: Once you roll your keys in Supabase, update this string!
   SUPABASE_ANON_KEY: 'sb_publishable_oZ1RQOpJ4BoIAq_vDAqHWw_lOnoqFo0',
   TARGET_TAMBAY_HOURS: 10,
   WEIGHTS: {
@@ -25,73 +26,4 @@ export const OFFICIAL_EVENTS_LIST = [
   { name: "Apps Mems Bonding 1", weightPercent: 5 },
   { name: "Apps Mems Bonding 2", weightPercent: 5 },
   { name: "Apps Only Bonding", weightPercent: 5 }
-];
-
-export const PES_LIST = [
-  {
-    roleKey: 'PRESIDENT',
-    title: 'President',
-    fullName: 'Jose Miguel P. Macatangay',
-    email: 'jpmacatangay1@up.edu.ph',
-    photo: 'assets/pes/macatangay.jpg'
-  },
-  {
-    roleKey: 'EVP',
-    title: 'Executive Vice President',
-    fullName: 'Mikaela E. Donato',
-    email: 'medonato@up.edu.ph',
-    photo: 'assets/pes/donato.jpg'
-  },
-  {
-    roleKey: 'SEC_GEN',
-    title: 'Secretary-General',
-    fullName: 'Jo Elise G. Gulle',
-    email: 'jggulle1@up.edu.ph',
-    photo: 'assets/pes/gulle.jpg'
-  }
-];
-
-export const COMMITTEES_LIST = [
-  {
-    name: 'Academics',
-    vpTitle: 'Vice President for Academic Affairs',
-    vp: 'Jian Christian F. Fermin',
-    vpEmail: 'jffermin@up.edu.ph',
-    photo: 'assets/vps/fermin.jpg'
-  },
-  {
-    name: 'Publicity',
-    vpTitle: 'Vice President for Publicity',
-    vp: 'Jannah Marc D. Morales',
-    vpEmail: 'jdmorales3@up.edu.ph',
-    photo: 'assets/vps/morales.jpg'
-  },
-  {
-    name: 'RAComm',
-    vpTitle: 'Vice President for Recruitment and Applications',
-    vp: 'Sandra Lee S. Polinar',
-    vpEmail: 'sspolinar1@up.edu.ph',
-    photo: 'assets/vps/polinar.jpg'
-  },
-  {
-    name: 'Internal',
-    vpTitle: 'Vice President for Internal Affairs',
-    vp: 'Cian Joseph M. Catimbang',
-    vpEmail: 'cmcatimbang@up.edu.ph',
-    photo: 'assets/vps/catimbang.jpg'
-  },
-  {
-    name: 'External',
-    vpTitle: 'Vice President for External Affairs',
-    vp: 'Queenie C. Necesario',
-    vpEmail: 'qcnecesario@up.edu.ph',
-    photo: 'assets/vps/necesario.jpg'
-  },
-  {
-    name: 'Finance',
-    vpTitle: 'Vice President for Finance',
-    vp: 'Regine Ann C. Reyes',
-    vpEmail: 'rcreyes18@up.edu.ph',
-    photo: 'assets/vps/reyes.jpg'
-  }
 ];
