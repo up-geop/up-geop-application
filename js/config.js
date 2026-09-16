8a8a336330cfb214d792fb73867e092ac8c14519
Chirpy1230 <erignacio1@up.edu.ph>
2026-09-16 14:16:09 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,53 @@
 export const CONFIG = {
   TARGET_TAMBAY_HOURS: 10,
-  MEMBER_SIGNATORY_LIMIT: 4,
   WEIGHTS: {
-    SIGNATORIES: 0.40,
-    TAMBAY: 0.30,
-    EVENTS: 0.30
+    SIGNATORIES: 0.5,
+    TAMBAY: 0.3,
+    EVENTS: 0.2
   }
 };
 
 export const COMMITTEES_LIST = [
-  { name: 'Academics', vp: 'VP for Academic Affairs' },
-  { name: 'Publicity', vp: 'VP for Publicity Affairs' },
-  { name: 'RAComm', vp: 'Recruitment & Applications Committee Head' },
-  { name: 'Internal', vp: 'VP for Internal Affairs' },
-  { name: 'External', vp: 'VP for External Affairs' },
-  { name: 'Finance', vp: 'VP for Finance Affairs' }
+  {
+    name: 'Academics',
+    vpTitle: 'Vice President for Academic Affairs',
+    vp: 'Jian Christian F. Fermin',
+    vpEmail: 'jffermin@up.edu.ph',
+    photo: 'assets/vps/fermin.jpg'
+  },
+  {
+    name: 'Publicity',
+    vpTitle: 'Vice President for Publicity',
+    vp: 'Jannah Marc D. Morales',
+    vpEmail: 'jdmorales3@up.edu.ph',
+    photo: 'assets/vps/morales.jpg'
+  },
+  {
+    name: 'RAComm',
+    vpTitle: 'Vice President for Recruitment and Applications',
+    vp: 'Sandra Lee S. Polinar',
+    vpEmail: 'sspolinar1@up.edu.ph',
+    photo: 'assets/vps/polinar.jpg'
+  },
+  {
+    name: 'Internal',
+    vpTitle: 'Vice President for Internal Affairs',
+    vp: 'Cian Joseph M. Catimbang',
+    vpEmail: 'cmcatimbang@up.edu.ph',
+    photo: 'assets/vps/catimbang.jpg'
+  },
+  {
+    name: 'External',
+    vpTitle: 'Vice President for External Affairs',
+    vp: 'Queenie C. Necesario',
+    vpEmail: 'qcnecesario@up.edu.ph',
+    photo: 'assets/vps/necesario.jpg'
+  },
+  {
+    name: 'Finance',
+    vpTitle: 'Vice President for Finance',
+    vp: 'Regine Ann C. Reyes',
+    vpEmail: 'rcreyes18@up.edu.ph',
+    photo: 'assets/vps/reyes.jpg'
+  }
 ];
